eda6793ebc823dceff3c7e2a2ee6ce00cea5e834
Ethan Obott <217574989+ECC06@users.noreply.github.com>
2025-11-19 15:50:01 +0000

    write simpler logic for adding elements and reading local storage

---
+++ javascript/lists-page.js
@@ -0,0 +1,165 @@
+const initialCreateListBtn = document.querySelector("#create-list-btn");
+const cancelCreateListBtn = document.querySelector("#cancel-create-list-btn");
+const nameListForm = document.querySelector("#add-item-form");
+const nameListDialogue = document.querySelector("dialog");
+const addListBtn = document.querySelector("#add-list-btn");
+const listsCont = document.querySelector("#lists-cont");
+
+const listFromLocalStorage = () => JSON.parse(localStorage.getItem("lists"));
+const localStorageEmpty = () => !localStorage.getItem("lists");
+
+const lastElemOfList = () => listsCont.lastElementChild; //gets the current last element of the list (it's changes as more and more are added)
+
+//object that will contain info about each list item
+const listObj = {};
+
+//!UTILITY FUNCTIONS (CALLED MULTIPLE TIMES)
+
+//clear the "list-name" input field
+const closeDialogAndClearInput = () => {
+	nameListForm.elements["list-name-input"].value = "";
+	nameListDialogue.close();
+};
+
+//hides the default page and shows the list
+function displayList() {
+	const listsAndAddButtonCont = document.querySelector(
+		"#lists-and-add-button-cont",
+	);
+	const noListsCont = document.querySelector("#no-lists-cont");
+	const header = document.querySelector("header");
+
+	noListsCont.classList.add("display-none"); //hide default page
+
+	header.classList.remove("display-none"); //show header
+	listsAndAddButtonCont.classList.remove("display-none"); //show lists
+	nameListDialogue.close();
+}
+
+//returns true if the name the user types in has been stored by them previously. otherwise, it returns false
+function userDuplicatedTitle(userInput) {
+	if (!localStorageEmpty()) {
+		for (const obj of listFromLocalStorage()) {
+			if (obj.userInput === userInput) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function populateListItem(listObj) {
+	lastElemOfList().id = listObj.id; //populate this single list item with the id of the single list object in local storage
+
+	lastElemOfList().classList.remove("display-none");
+
+	//extracts the elements containing the list element's name and date of creation from a the first div in the last list item
+	const [listNameElem, dateCreatedElem] =
+		lastElemOfList().firstElementChild.children;
+
+	//updates the inner text of the newly added list element with properties the last object in local storage
+	listNameElem.innerText = listObj.userInput;
+	dateCreatedElem.innerText = listObj.dateOfCreation;
+}
+
+//!EVENT HANDLERS
+
+//button that is shown on the "no lists yet" page
+initialCreateListBtn.addEventListener("click", () => {
+	nameListDialogue.showModal();
+});
+
+//if local storage is not empty, this handler fetches the array of lists from local storage and displays it as HTML on the page
+document.addEventListener("DOMContentLoaded", () => {
+	const listCont = document.querySelector(".list-cont");
+	listsCont.innerHTML = "";
+
+	if (!localStorageEmpty()) {
+		listFromLocalStorage().forEach((obj) => {
+			//each iteration appends a clone of listCont so that listCont doesn't get moved every time appendChild is called
+			listsCont.appendChild(listCont.cloneNode(true));
+			populateListItem(obj);
+			displayList();
+		});
+	}
+});
+
+//when submitted, a new list created
+nameListForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const nameOfList = nameListForm.elements["list-name-input"].value;
+
+	// Check for duplicates before storing
+	if (userDuplicatedTitle(nameOfList)) {
+		alert(`You already have a list named ${nameOfList}!`);
+		closeDialogAndClearInput();
+		return; //terminates the function
+	} else {
+		listObj["userInput"] = nameOfList;
+	}
+
+	storeList();
+	addListToHTML();
+	closeDialogAndClearInput();
+});
+
+//when clicked, the form for naming a list is shown
+addListBtn.addEventListener("click", () => {
+	//clear the "list-name" input field
+	nameListDialogue.showModal();
+});
+
+//when clicked, the form for naming a list is closed
+cancelCreateListBtn.addEventListener("click", () => {
+	closeDialogAndClearInput();
+});
+
+//stores the user's input in local storage and then displays it to the user
+function storeList() {
+	//store last modified
+	const lastModified = null; //list hasn't been modified yet
+	listObj["lastModified"] = lastModified;
+
+	//store list id
+	const listId = Math.floor(Math.random() * 1000);
+	listObj["id"] = listId;
+
+	//store date of creation
+	const dateObj = new Date();
+	const month = dateObj.toLocaleString("default", { month: "short" });
+	const day = dateObj.getDate();
+	const year = dateObj.getFullYear();
+	const dateOfCreation = `Created: ${month} ${day} ${year}`;
+	listObj["dateOfCreation"] = dateOfCreation;
+
+	updateLocalStorage();
+
+	function updateLocalStorage() {
+		if (localStorageEmpty()) {
+			localStorage.setItem("lists", JSON.stringify([listObj]));
+		} else {
+			const arrOfLists = JSON.parse(localStorage.getItem("lists"));
+			arrOfLists.push(listObj);
+
+			localStorage.setItem("lists", JSON.stringify(arrOfLists));
+		}
+	}
+}
+
+//adds a new item to the list container and populates it with the newly added list data (e.g the list name the user just typed in)
+function addListToHTML() {
+	const lastObjInLocalStorage =
+		listFromLocalStorage()[listFromLocalStorage().length - 1];
+
+	if (listFromLocalStorage().length === 1) {
+		populateListItem(); //update the newly added item with the right information
+		displayList(); //display list container for the first time
+	} else {
+		//if there are some lists in the container
+		const cloned = lastElemOfList().cloneNode(true); //clone the last item in the list
+		listsCont.appendChild(cloned); //add it to the list
+		populateListItem(lastObjInLocalStorage);
+	}
+}
